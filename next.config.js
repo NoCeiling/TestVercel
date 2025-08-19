71d9fed18a542490757d56b1e855bfd0d1d56ef0
NoCeiling <825846592@QQ.COM>
2025-08-19 23:36:02 +0800

    修复问题2

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/guides',
-        destination: '/guides/',
-        permanent: true,
-      },
-      {
-        source: '/varieties',
-        destination: '/varieties/',
-        permanent: true,
-      },
-    ];
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
